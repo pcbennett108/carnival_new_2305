baef6edd71a620d4004167c1a1c938979330c839
pcbennett108 <pcbennett42@gmail.com>
2023-06-13 11:54:40 -0600

    Add test fro total revenue

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -9,6 +9,9 @@ RSpec.describe Carnival do
     @ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
     @ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
     @ride3 = Ride.new({ name: 'Roller Coaster', min_height: 54, admission_fee: 2, excitement: :thrilling })
+    @visitor1 = Visitor.new('Bruce', 54, '$10')
+    @visitor2 = Visitor.new('Tucker', 36, '$5')
+    @visitor3 = Visitor.new('Penny', 64, '$15')
 
   end
 
@@ -27,6 +30,44 @@ RSpec.describe Carnival do
     @carnival1.add_ride(@ride2)
     @carnival1.add_ride(@ride3)
     expect(@carnival1.rides).to eq([@ride1, @ride2, @ride3])
+  end
+  
+  it "can return total revenue" do
+    @carnival1.add_ride(@ride1)
+    @carnival1.add_ride(@ride2)
+    @carnival1.add_ride(@ride3)
+    @visitor1.add_preference(:gentle)
+    @visitor2.add_preference(:gentle)
+    @visitor2.add_preference(:thrilling)
+    @visitor3.add_preference(:thrilling)
+    @ride1.board_rider(@visitor1)
+    @ride1.board_rider(@visitor2)
+    @ride1.board_rider(@visitor1)
+    @ride2.board_rider(@visitor1)
+    @ride2.board_rider(@visitor2)
+    @ride3.board_rider(@visitor1)
+    @ride3.board_rider(@visitor2)
+    @ride3.board_rider(@visitor3)
+    expect(@carnival1.total_revenue).to eq(15)
+  end
+
+  xit "can determin the most popular ride" do
+    @carnival1.add_ride(@ride1)
+    @carnival1.add_ride(@ride2)
+    @carnival1.add_ride(@ride3)
+    @visitor1.add_preference(:gentle)
+    @visitor2.add_preference(:gentle)
+    @visitor2.add_preference(:thrilling)
+    @visitor3.add_preference(:thrilling)
+    @ride1.board_rider(@visitor1)
+    @ride1.board_rider(@visitor2)
+    @ride1.board_rider(@visitor1)
+    @ride2.board_rider(@visitor1)
+    @ride2.board_rider(@visitor2)
+    @ride3.board_rider(@visitor1)
+    @ride3.board_rider(@visitor2)
+    @ride3.board_rider(@visitor3)
+    expect(@carnival1.most_popular_ride).to eq(@ride1)
   end
   
 
