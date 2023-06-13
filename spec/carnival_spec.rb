f5b61615cc0367e723f2986e6d33636b3e26a97b
pcbennett108 <pcbennett42@gmail.com>
2023-06-13 11:38:44 -0600

    Add Carnival class with add_ride method

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -5,7 +5,7 @@ require './lib/carnival'
 
 RSpec.describe Carnival do 
   before(:each) do
-    @carnival1 = Carnival.new(9)
+    @carnival1 = Carnival.new("Funzday", 9)
     @ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
     @ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
     @ride3 = Ride.new({ name: 'Roller Coaster', min_height: 54, admission_fee: 2, excitement: :thrilling })
@@ -13,18 +13,19 @@ RSpec.describe Carnival do
   end
 
   it "exists" do
-    expect(@carnival).to be_instance_of(Carnival)
+    expect(@carnival1).to be_instance_of(Carnival)
   end
 
   it "has attributes" do
+    expect(@carnival1.name).to eq("Funzday")
     expect(@carnival1.duration).to eq(9)
     expect(@carnival1.rides).to eq([])
   end
   
   it "can add rides" do
-    @carnival.add_ride(@ride1)
-    @carnival.add_ride(@ride2)
-    @carnival.add_ride(@ride3)
+    @carnival1.add_ride(@ride1)
+    @carnival1.add_ride(@ride2)
+    @carnival1.add_ride(@ride3)
     expect(@carnival1.rides).to eq([@ride1, @ride2, @ride3])
   end
   
